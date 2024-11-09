fb459a25ec7085759480183dba3a080345fbc596
Fabricio Cosati <fabricio18cosati@gmail.com>
2024-11-09 06:55:55 -0300

    Cofiguring docker-compose & mysql table migration

---
+++ migrations/order.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS onflydb;
+USE onflydb;
+
+CREATE TABLE `onflydb`.`orders` (
+  `id_order` INT NOT NULL AUTO_INCREMENT,
+  `requester_name_order` VARCHAR(255) NOT NULL,
+  `destination_order` VARCHAR(255) NOT NULL,
+  `going_date_order` TIMESTAMP NOT NULL,
+  `return_date_order` TIMESTAMP NOT NULL,
+  `status_order` ENUM("requested", "approved", "canceled") NOT NULL,
+  `created_at_order` DATETIME NOT NULL DEFAULT NOW(),
+  `updated_at_order` DATETIME NOT NULL DEFAULT NOW(),
+  PRIMARY KEY (`id_order`));
