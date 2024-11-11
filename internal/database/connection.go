6c9583cd023726cd6d195fdbb9e6f474c9e9d879
Fabricio Cosati <fabricio18cosati@gmail.com>
2024-11-11 03:06:10 -0300

    Try to connect database 5 times to init app

--- internal/database/connection.go
+++ internal/database/connection.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/FabricioCosati/onfly-test/internal/config"
 	"gorm.io/driver/mysql"
@@ -9,9 +10,20 @@ import (
 )
 
 func ConnectToDatabase(config config.Config) (*gorm.DB, error) {
+	var db *gorm.DB
+	var err error
+
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=%s",
 		config.DbUser, config.DbPass, config.DbHost, config.DbPort, config.DbName, config.TimeZone,
 	)
 
-	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	for i := 0; i < 5; i++ {
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err != nil {
+			time.Sleep(8 * time.Second)
+			continue
+		}
+		break
+	}
+	return db, err
 }
